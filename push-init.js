363fba4c414fe3556451ce4e2e27e6e89b3d86e8
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-01-31 12:47:57 +0100

    Update push-init.js

--- push-init.js
+++ push-init.js
@@ -2,14 +2,13 @@ window.OneSignalDeferred = window.OneSignalDeferred || [];
 
 OneSignalDeferred.push(async function(OneSignal) {
     // 1. Initialisierung
-    await OneSignal.init({
-        appId: "4fd0fec9-a8dd-40d9-94e9-7ba330c07176",
-        serviceWorkerPath: "/push/onesignal/OneSignalSDKWorker.js",
-        serviceWorkerUpdaterPath: "/push/onesignal/OneSignalSDKUpdaterWorker.js",
-        serviceWorkerParam: {
-            scope: "/sportschuetzen/push/onesignal/"
-        }
-    });
+ await OneSignal.init({
+    appId: "4fd0fec9-a8dd-40d9-94e9-7ba330c07176",
+    serviceWorkerPath: "/sportschuetzen/push/onesignal/OneSignalSDKWorker.js",
+    serviceWorkerUpdaterPath: "/sportschuetzen/push/onesignal/OneSignalSDKUpdaterWorker.js",
+    serviceWorkerParam: { scope: "/sportschuetzen/push/onesignal/" }
+});
+
     console.log("OneSignal Init OK");
 
     // 2. Prüfung der Berechtigung & Hinweis-Banner
