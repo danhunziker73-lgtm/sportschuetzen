d9afc8e80722c2aa5ab445bb7955c4e0e3e072d1
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-01-31 12:38:42 +0100

    Update push-init.js

--- push-init.js
+++ push-init.js
@@ -4,8 +4,8 @@ OneSignalDeferred.push(async function(OneSignal) {
     // 1. Initialisierung
     await OneSignal.init({
         appId: "4fd0fec9-a8dd-40d9-94e9-7ba330c07176",
-        serviceWorkerPath: "/sportschuetzen/push/onesignal/OneSignalSDKWorker.js",
-        serviceWorkerUpdaterPath: "/sportschuetzen/push/onesignal/OneSignalSDKUpdaterWorker.js",
+        serviceWorkerPath: "/push/onesignal/OneSignalSDKWorker.js",
+        serviceWorkerUpdaterPath: "/push/onesignal/OneSignalSDKUpdaterWorker.js",
         serviceWorkerParam: {
             scope: "/sportschuetzen/push/onesignal/"
         }
