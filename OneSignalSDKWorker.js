d212fcd60b991cb39497a8ccb25c4bf2df28b66e
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-01-31 20:54:44 +0100

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,9 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+// WICHTIG: Dies MUSS die allererste Zeile sein!
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+
+// Erst danach darf dein eigener Code kommen
+try {
+    importScripts('/sportschuetzen/sw.js');
+} catch (e) {
+    console.error("Eigener SW konnte nicht geladen werden:", e);
+}
