a0f69e034cd3c057d00304c776f80a688f73e110
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-01-31 21:33:23 +0100

    Update push-init.js

--- push-init.js
+++ push-init.js
@@ -17,8 +17,8 @@ OneSignalDeferred.push(async function(OneSignal) {
 await OneSignal.init({
     appId: "4fd0fec9-a8dd-40d9-94e9-7ba330c07176",
     // Hier die Pfade wieder in den Unterordner setzen:
-    serviceWorkerPath: "push/onesignal/OneSignalSDKWorker.js",
-    serviceWorkerUpdaterPath: "push/onesignal/OneSignalSDKUpdaterWorker.js",
+    serviceWorkerPath: "OneSignalSDKWorker.js",
+    serviceWorkerUpdaterPath: "OneSignalSDKUpdaterWorker.js",
     // Der Scope muss aber das Hauptverzeichnis sein, damit die Links funktionieren!
     serviceWorkerParam: { scope: "/sportschuetzen/" }
 });
