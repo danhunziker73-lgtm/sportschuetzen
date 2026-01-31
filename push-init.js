0ba0d35ff56f02728551d94935f087f69cf1595e
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-01-31 21:24:15 +0100

    Update push-init.js

--- push-init.js
+++ push-init.js
@@ -14,13 +14,14 @@ function getIosStatus() {
 
 // 1. OneSignal Initialisierung
 OneSignalDeferred.push(async function(OneSignal) {
-    await OneSignal.init({
-        appId: "4fd0fec9-a8dd-40d9-94e9-7ba330c07176",
-        serviceWorkerPath: "OneSignalSDKWorker.js",
-        serviceWorkerUpdaterPath: "OneSignalSDKUpdaterWorker.js",
-        serviceWorkerParam: { scope: "/sportschuetzen/" }
-    });
-
+await OneSignal.init({
+    appId: "4fd0fec9-a8dd-40d9-94e9-7ba330c07176",
+    // Hier die Pfade wieder in den Unterordner setzen:
+    serviceWorkerPath: "push/onesignal/OneSignalSDKWorker.js",
+    serviceWorkerUpdaterPath: "push/onesignal/OneSignalSDKUpdaterWorker.js",
+    // Der Scope muss aber das Hauptverzeichnis sein, damit die Links funktionieren!
+    serviceWorkerParam: { scope: "/sportschuetzen/" }
+});
     console.log("OneSignal Init OK");
 
     // NAVIGATION-FIX: Wenn die App offen ist und ein Push geklickt wird
