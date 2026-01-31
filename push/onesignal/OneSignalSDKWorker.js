7dad35f89c35a6f14d0ded1de31990f9ae06aa1f
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-01-31 21:32:48 +0100

    Update and rename push/onesignal/OneSignalSDKWorker.js to OneSignalSDKWorker.js

--- push/onesignal/OneSignalSDKWorker.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
-
-// Falls deine sw.js im Hauptordner liegt, brauchen wir ../..
-try {
-    importScripts('../../sw.js');
-} catch (e) {
-    console.error("Zusatz-SW nicht gefunden");
-}
