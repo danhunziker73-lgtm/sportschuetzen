17ada35dd07f8476e32374b46eddf392046c139e
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-02-01 14:19:24 +0100

    Update push-init.js

--- push-init.js
+++ push-init.js
@@ -6,6 +6,7 @@ await OneSignal.init({
   appId: "975091f5-948b-48d4-9e61-b7f4d43a1021",
   serviceWorkerPath: "push/onesignal/OneSignalSDKWorker.js",
   serviceWorkerUpdaterWorkerPath: "push/onesignal/OneSignalSDKUpdaterWorker.js"
+  serviceWorkerScope: "/"  // <- scope auf Root setzen
 });
     console.log("✅ OneSignal bereit", {
       optedIn: OneSignal.User.PushSubscription.optedIn,
