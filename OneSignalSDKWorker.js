85e362a4c8395806a01d234f8786be71c80c3a26
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-02-01 13:18:49 +0100

    Create OneSignalSDKWorker.js

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,15 @@
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+  const url = event.notification?.data?.url || "/";
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true })
+      .then(clientList => {
+        for (const client of clientList) {
+          if ("focus" in client) return client.focus();
+        }
+        return clients.openWindow(url);
+      })
+  );
+});
