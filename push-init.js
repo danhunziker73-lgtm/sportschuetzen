6d1c050f39983f7e21306777ed13fd26dda779c4
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-02-01 13:38:54 +0100

    Update push-init.js

--- push-init.js
+++ push-init.js
@@ -2,12 +2,11 @@ if (window === window.top) {
   window.OneSignalDeferred = window.OneSignalDeferred || [];
 
   OneSignalDeferred.push(async (OneSignal) => {
-    await OneSignal.init({
-      appId: "975091f5-948b-48d4-9e61-b7f4d43a1021",
-      serviceWorkerPath: "OneSignalSDKWorker.js",
-      serviceWorkerUpdaterWorkerPath: "OneSignalSDKUpdaterWorker.js"
-    });
-
+await OneSignal.init({
+  appId: "975091f5-948b-48d4-9e61-b7f4d43a1021",
+  serviceWorkerPath: "push/onesignal/OneSignalSDKWorker.js",
+  serviceWorkerUpdaterWorkerPath: "push/onesignal/OneSignalSDKUpdaterWorker.js"
+});
     console.log("✅ OneSignal bereit", {
       optedIn: OneSignal.User.PushSubscription.optedIn,
       permission: await OneSignal.Notifications.permission
