5372293190f3a71b2eeab98e6c52e65370ef1dc5
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-02-04 18:08:16 +0100

    Create onesignal.js

---
+++ js/onesignal.js
@@ -0,0 +1,18 @@
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "975091f5-948b-48d4-9e61-b7f4d43a1021",
+      autoRegister: false,
+      notifyButton: { enable: true }
+    });
+
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
+
+    if (isIOS && !isStandalone) {
+      console.log("Bitte App zum Homescreen hinzufügen");
+    }
+  });
+</script>
