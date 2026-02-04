39fba4786531b7d697edf950ce0e3801e8a30146
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-02-04 17:25:23 +0100

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,14 +1,1 @@
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
-
-self.addEventListener('push', event => {
-    console.log("🛎 push event", event);
-});
-
-self.addEventListener('notificationclick', event => {
-    console.log("🖱 notificationclick event", event);
-    event.notification.close();
-});
-
-self.addEventListener('notificationclose', event => {
-    console.log("❌ notificationclose event", event);
-});
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
