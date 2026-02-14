80d79337a42bad063d3ad7a53c93cd84df08db1a
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-02-14 07:10:59 +0100

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,3 +1,5 @@
-self.addEventListener("message", () => {});
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+// MUSS als allererstes kommen
+self.addEventListener("message", function () {});
 
+// OneSignal laden
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
