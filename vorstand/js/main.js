70c7a2c6a66487a65629ccc26558b470b251b975
danhunziker73-lgtm <dan.hunziker73@gmail.com>
2026-02-15 19:14:53 +0100

    Update main.js

--- vorstand/js/main.js
+++ vorstand/js/main.js
@@ -40,7 +40,7 @@ function navTo(viewId) {
 
     // Module laden
     if (viewId === 'inventar' && typeof loadInventarData === 'function') loadInventarData();
-    if (viewId === 'grenzland' && typeof loadGrenzlandData === 'function') loadGrenzlandData();
+    if (viewId === 'manager' && typeof loadmanagerData === 'function') loadmanagerData();
     if (viewId === 'termine' && typeof loadTermineData === 'function') loadTermineData();
 }
 
